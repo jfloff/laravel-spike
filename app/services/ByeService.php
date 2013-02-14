62154ed6aa8de495c4445ff9a42a6a4bb9d8b591
João Ferreira Loff <jfloff@gmail.com>
2013-02-14 22:59:03 +0000

    Changed services to write to a file 5 seconds interval

--- app/services/ByeService.php
+++ app/services/ByeService.php
@@ -7,6 +7,7 @@ class ByeService {
         $name = $data['name'];
 
         $handle = fopen('/tmp/services.txt', 'a+');
+        sleep(5);
         $date = date('Y-m-d H:i:s');
         fwrite($handle, "[$date] Bye $name! :(\n");
 
--- app/services/HelloService.php
+++ app/services/HelloService.php
@@ -7,6 +7,7 @@ class HelloService {
         $name = $data['name'];
 
         $handle = fopen('/tmp/services.txt', 'a+');
+        sleep(5);
         $date = date('Y-m-d H:i:s');
         fwrite($handle, "[$date] Hello $name! :)\n");
 
